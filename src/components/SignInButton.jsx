88733a335f5e9bc31f4e58ea1f50f425b0027562
George Sproston <gsproston@scottlogic.com>
2022-09-05 11:36:56 +0100

    Using redirects rather than pop ups

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -4,13 +4,13 @@ import { loginRequest } from "../authConfig";
 import Button from "react-bootstrap/Button";
 
 function handleLogin(instance) {
-  instance.loginPopup(loginRequest).catch((e) => {
+  instance.loginRedirect(loginRequest).catch((e) => {
     console.error(e);
   });
 }
 
 /**
- * Renders a button which, when selected, will open a popup for login
+ * Renders a button which, when selected, will redirect the page to the login prompt
  */
 export const SignInButton = () => {
   const { instance } = useMsal();
@@ -21,7 +21,7 @@ export const SignInButton = () => {
       className="ml-auto"
       onClick={() => handleLogin(instance)}
     >
-      Sign in using Popup
+      Sign in using Redirect
     </Button>
   );
 };
--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -3,13 +3,13 @@ import { useMsal } from "@azure/msal-react";
 import Button from "react-bootstrap/Button";
 
 function handleLogout(instance) {
-  instance.logoutPopup().catch((e) => {
+  instance.logoutRedirect().catch((e) => {
     console.error(e);
   });
 }
 
 /**
- * Renders a button which, when selected, will open a popup for logout
+ * Renders a button which, when selected, will redirect the page to the logout prompt
  */
 export const SignOutButton = () => {
   const { instance } = useMsal();
@@ -20,7 +20,7 @@ export const SignOutButton = () => {
       className="ml-auto"
       onClick={() => handleLogout(instance)}
     >
-      Sign out using Popup
+      Sign out using Redirect
     </Button>
   );
 };
